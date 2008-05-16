f76e7f211a943d342830efda913319f79fb8e8f4
Carlos Brando <eduardobrando@gmail.com>
2008-05-16 01:38:38 -0300

    Version 1.0.0

--- lib/autotest_notification/version.rb
+++ lib/autotest_notification/version.rb
@@ -1,7 +1,7 @@
 module AutotestNotification #:nodoc:
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 3
+    MAJOR = 1
+    MINOR = 0
     TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
