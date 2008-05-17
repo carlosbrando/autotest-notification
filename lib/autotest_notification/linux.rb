6e193044506b7bd18f3204652ea309c9ac0e929e
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2008-05-17 00:35:39 -0300

    linux speach using espeak

--- lib/autotest_notification/linux.rb
+++ lib/autotest_notification/linux.rb
@@ -9,33 +9,43 @@ module AutotestNotification
         elsif has_kdialog?
           kdialog(title, msg, img)
         end
+        talk(msg)
         true # true, cause we did everything we could
       end
 
       protected
-        def notify_send(title, msg, img)
-          system "notify-send -t #{EXPIRATION_IN_SECONDS * 1000} -i #{img} '#{title}' '#{msg}'"
-        end
+      def notify_send(title, msg, img)
+        system "notify-send -t #{EXPIRATION_IN_SECONDS * 1000} -i #{img} '#{title}' '#{msg}'"
+      end
 
-        def kdialog(title, msg, img)
-          system "kdialog --title '#{title}' --passivepopup '#{msg}' #{EXPIRATION_IN_SECONDS}"
-        end
+      def kdialog(title, msg, img)
+        system "kdialog --title '#{title}' --passivepopup '#{msg}' #{EXPIRATION_IN_SECONDS}"
+      end
 
-        def zenity(title, msg, img)
-          system "zenity --info --text='#{msg}' --title='#{title}'"
-        end
+      def zenity(title, msg, img)
+        system "zenity --info --text='#{msg}' --title='#{title}'"
+      end
 
-        def has_zenity?
-          system "which zenity 2> /dev/null"
+      def talk(msg)
+        begin
+          f = File.new('/usr/bin/espeak')
+          system("/usr/bin/espeak '#{msg}'")  if SPEAKING && failures > 0
+        rescue
         end
+      end
 
-        def has_notify?
-          system "which notify-send 2> /dev/null"
-        end
+      def has_zenity?
+        system "which zenity 2> /dev/null"
+      end
 
-        def has_kdialog?
-          system "which kdialog 2> /dev/null"
-        end
+      def has_notify?
+        system "which notify-send 2> /dev/null"
+      end
+
+      def has_kdialog?
+        system "which kdialog 2> /dev/null"
+      end
     end
   end
 end
+
