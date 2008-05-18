e9e2f86169e512142f6d4d936c5f561b82d0d9f1
Carlos Brando <eduardobrando@gmail.com>
2008-05-18 14:36:15 -0300

    Fix test.

--- test/autotest_notification/test_mac.rb
+++ test/autotest_notification/test_mac.rb
@@ -11,7 +11,7 @@ class TestMac < Test::Unit::TestCase
   def test_notify_with_voice
     Object.const_set :SPEAKING, true
     AutotestNotification::Mac.expects(:system).with("growlnotify -n autotest --image image -p 0 -m 'msg' title")
-    AutotestNotification::Mac.expects(:system).with("saya 1 test failed.")
+    AutotestNotification::Mac.expects(:system).with("say 1 test failed.")
     AutotestNotification::Mac.notify("title", "msg", "image", 1)
   end
 
