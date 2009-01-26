60be2c8e4bd8db679c852f689c97973085f20ceb
Diego Carrion <dc.rec1@gmail.com>
2009-01-25 23:22:41 -0200

    added an-install options: --success-sound and --failure-sound

--- lib/autotest_notification/mac.rb
+++ lib/autotest_notification/mac.rb
@@ -6,11 +6,12 @@ module AutotestNotification
       
       def notify(title, msg, img, total = 1, failures = 0, priority = 0)
         system "growlnotify -n autotest --image #{img} #{'-s ' if ((failures > 0) and STICKY)}-p #{priority} -m '#{msg}' -t #{title}"
+        play(SUCCESS_SOUND) unless SUCCESS_SOUND.empty? or failures > 0
+        play(FAILURE_SOUND) unless FAILURE_SOUND.empty? or failures == 0
         say(total, failures) if SPEAKING
       end
 
       def say(total, failures)
-
         if failures > 0
           DOOM_EDITION ? Doom.play_sound(total, failures) : system("say #{failures} test#{'s' unless failures == 1} failed.")
           @last_test_failed = true
@@ -18,6 +19,10 @@ module AutotestNotification
           DOOM_EDITION ? Doom.play_sound(total, failures) : system("say All tests passed successfully.")
           @last_test_failed = false
         end
+      end
+      
+      def play(sound_file)
+        `#{File.expand_path(File.dirname(__FILE__) + "/../../bin/")}/playsound #{sound_file}`
       end
       
     end
--- test/autotest_notification/test_mac.rb
+++ test/autotest_notification/test_mac.rb
@@ -7,6 +7,9 @@ class TestMac < Test::Unit::TestCase
     AutotestNotification.const_set :SPEAKING, false
     AutotestNotification.const_set :STICKY, false
     AutotestNotification.const_set :BUUF, false
+    AutotestNotification.const_set :SUCCESS_SOUND, ''
+    AutotestNotification.const_set :FAILURE_SOUND, ''
+    AutotestNotification::Mac.stubs(:system)
   end
 
   def test_notify_without_voice
@@ -34,9 +37,42 @@ class TestMac < Test::Unit::TestCase
     AutotestNotification::Mac.notify("title", "msg", "image", 1, 1)
   end
   
+  def test_success_sound_is_played_when_specified_and_test_success
+    AutotestNotification.const_set :SUCCESS_SOUND, sound_path
+    AutotestNotification::Mac.expects(:play).with(sound_path)
+    AutotestNotification::Mac.notify("title", "msg", "image", 1, 0)
+  end
+  
+  def test_failure_sound_is_played_when_specified_and_tests_fail
+    AutotestNotification.const_set :FAILURE_SOUND, sound_path
+    AutotestNotification::Mac.expects(:play).with(sound_path)
+    AutotestNotification::Mac.notify("title", "msg", "image", 1, 1)
+  end
+  
+  def test_failure_sound_is_not_played_when_tests_success
+    AutotestNotification.const_set :FAILURE_SOUND, sound_path
+    AutotestNotification::Mac.expects(:play).with(sound_path).times(0)
+    AutotestNotification::Mac.notify("title", "msg", "image", 1, 0)
+  end
+  
+  def test_success_sound_is_not_played_when_tests_fail
+    AutotestNotification.const_set :SUCCESS_SOUND, sound_path
+    AutotestNotification::Mac.expects(:play).with(sound_path).times(0)
+    AutotestNotification::Mac.notify("title", "msg", "image", 1, 1)
+  end
+  
+  def test_play_method_exists
+    # this should be testing if playsound is being called
+    assert_respond_to AutotestNotification::Mac, :play
+  end
+  
   def test_notify_with_buuf
     AutotestNotification.const_set :BUUF, true
     AutotestNotification::Mac.expects(:system).with("growlnotify -n autotest --image image -p 0 -m 'msg' -t title")
     AutotestNotification::Mac.notify("title", "msg", "image", 1, 1)
   end
+end
+
+def sound_path
+  "#{File.expand_path(File.dirname(__FILE__) + "/../../sounds/doom/")}/1.wav"
 end
