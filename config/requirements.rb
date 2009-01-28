54e19ce68b6505da7c2ba1e7cd13d188ed3b778f
Jens Wille <jens.wille@uni-koeln.de>
2009-01-28 14:38:13 +0100

    Load correct ZenTest library: ZenTest.rb had been renamed to zentest.rb back in version 3.1.0.

--- config/requirements.rb
+++ config/requirements.rb
@@ -4,7 +4,7 @@ include FileUtils
 require 'rubygems'
 %w[rake hoe newgem rubigen ZenTest].each do |req_gem|
   begin
-    require req_gem
+    require req_gem.downcase
   rescue LoadError
     puts "This Rakefile requires the '#{req_gem}' RubyGem."
     puts "Installation: gem install #{req_gem} -y"
