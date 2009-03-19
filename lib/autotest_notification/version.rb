de28ad8f5cec2dd05ba543901a7bf10bc89abe4a
Carlos Brando <eduardobrando@gmail.com>
2009-03-19 11:33:23 -0300

    Bump to 1.9.0 version

--- lib/autotest_notification/version.rb
+++ lib/autotest_notification/version.rb
@@ -1,8 +1,8 @@
 module AutotestNotification #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 8
-    TINY  = 2
+    MINOR = 9
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
