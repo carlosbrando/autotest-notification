af9986c4b650c98905fa0e518b96e3071a071e06
Carlos Brando <carlosbrando@ip-189-99-90-157.user.vivozap.com.br>
2009-08-24 12:05:59 -0300

    Bump to 2.0.0 - Ruby 1.9 working.

--- lib/autotest_notification/version.rb
+++ lib/autotest_notification/version.rb
@@ -1,8 +1,8 @@
 module AutotestNotification #:nodoc:
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 9
-    TINY  = 1
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
